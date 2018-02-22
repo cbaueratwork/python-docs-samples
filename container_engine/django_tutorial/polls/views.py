8997a2fd5cbc4ccda6a59450373bffc2aeb17058
Chris Bauer <bauerpower@google.com>
2018-02-22 13:01:06 -0800

    stackdriver and stderr logs

--- container_engine/django_tutorial/polls/views.py
+++ container_engine/django_tutorial/polls/views.py
@@ -11,17 +11,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import logging
 import random
 
+import logging
+import logging.handlers
+import google.cloud.logging as gcp_logging
+
+from google.oauth2 import service_account
 from django.http import HttpResponse
 
+cred = service_account.Credentials.from_service_account_file('./logging-key.json')
+gcp_logging_client = gcp_logging.Client(project='gke-python-2', credentials=cred)
+handler = gcp_logging_client.get_default_handler()
+logger = logging.getLogger('myapp-logs')
+gcp_logger = gcp_logging_client.logger('sd-logs')
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 def index(request):
     if random.randint(1,4) > 2:
-      logging.error('about to crash!')
+      logger.error('about to crash!')
+      gcp_logger.log_text('stackdriver is about to crash', labels={'test': 'failure'})
       raise ValueError('Oh no, an invalid value')
 
-    logging.info('got request for root')
-    logging.warn('AHHHHH got request for root')
-    return HttpResponse("Hello, world, modified. You're at the polls index.")
+    logger.info('it appears to be working')
+    gcp_logger.log_text('it appears to be working - sd', severity='INFO')
+    logger.warn('yikes this is a warning')
+    gcp_logger.log_text('yikes this is a warning - sd', severity='WARNING')
+    return HttpResponse("Hello, world, customized. You're at the polls index.")
